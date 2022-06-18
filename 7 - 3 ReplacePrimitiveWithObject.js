7c9db08bbf84496701108df992fcd0159147fc99
Jorge Soria <jsoria_s@yahoo.com>
2022-06-18 16:05:49 -0500

    added refactor 7 - 3

---
+++ 7 - 3 ReplacePrimitiveWithObject.js
@@ -0,0 +1,52 @@
+/* 
+
+MOTIVATION
+
+class Order…
+
+  constructor(data) {
+    this.priority = data.priority;
+    // more initialization
+
+Some client codes uses it like this:
+
+client…
+
+  highPriorityCount = orders.filter(o => "high" === o.priority
+                                      || "rush" === o.priority)
+                            .length;
+
+*/
+
+class Priority {
+    constructor(value) {
+        if (value instanceof Priority) return value;
+        if (Priority.legalValues().includes(value))
+            this._value = value;
+        else
+            throw new Error(`<${value}> is invalid for Priority`);
+    }
+    toString() {return this._value;}
+    get _index() {return Priority.legalValues().findIndex(s => s === this._value);}
+    static legalValues() {return ['low', 'normal', 'high', 'rush'];}
+    equals(other) {return this._index === other.index;}
+    higherThan(other) {return this._index > other.index;}
+    lowerThan(other) {return this._index < other.index;}
+}
+
+
+class Order {
+    constructor(data) {
+        this._priority = new Priority(data);
+    }
+    get priority() {return this._priority;}
+    get priorityString() {return this._priority.toString();}
+    set priority(aString) {this._priority = new Priority(aString);}
+}
+
+// Client...
+highPriorityCount = orders.filter(
+    o => o.priority.higherThan(new Priority("normal"))).length;
+
+
+
